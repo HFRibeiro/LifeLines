12dabebe58276e80a703a90b77ec303caeff3f26
SirRibeiro <ribeirh02@gmail.com>
2018-02-01 17:47:06 +0000

    add MM2 Android

---
+++ Android/LifeLinesMM2/app/src/main/java/com/lifelines/code/lifelinesmm2/AsyncConnection.java
@@ -0,0 +1,132 @@
+package com.lifelines.code.lifelinesmm2;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class AsyncConnection extends AsyncTask<Void, String, Exception> {
+    private String url;
+    private int port;
+    private int timeout;
+    private MainActivity connectionHandler;
+
+    private BufferedReader in;
+    private BufferedWriter out;
+    private Socket socket;
+    private boolean interrupted = false;
+
+    private String TAG = "";
+
+    public AsyncConnection(String url, int port, int timeout, MainActivity connectionHandler) {
+        this.url = url;
+        this.port = port;
+        this.timeout = timeout;
+        this.connectionHandler = connectionHandler;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Exception result) {
+        super.onPostExecute(result);
+        Log.d(TAG, "Finished communication with the socket. Result = " + result);
+        //TODO If needed move the didDisconnect(error); method call here to implement it on UI thread.
+    }
+
+    @Override
+    protected Exception doInBackground(Void... params) {
+        Exception error = null;
+
+        try {
+            Log.d(TAG, "Opening socket connection.");
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(url.replace("/",""), port), timeout);
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            connectionHandler.didConnect();
+            String dataReceived = "";
+            while(!interrupted) {
+                String line = in.readLine();
+                connectionHandler.didReceiveData(line);
+                /*
+                if(line!='\r') dataReceived+= line;
+                else
+                {
+                    connectionHandler.didReceiveData(dataReceived);
+                    dataReceived = "";
+                }
+                */
+
+            }
+        } catch (UnknownHostException ex) {
+            Log.e(TAG, "doInBackground(): " + ex.toString());
+            error = interrupted ? null : ex;
+        } catch (IOException ex) {
+            Log.d(TAG, "doInBackground(): " + ex.toString());
+            error = interrupted ? null : ex;
+        } catch (Exception ex) {
+            Log.e(TAG, "doInBackground(): " + ex.toString());
+            error = interrupted ? null : ex;
+        } finally {
+            try {
+                socket.close();
+                out.close();
+                in.close();
+            } catch (Exception ex) {}
+        }
+
+        connectionHandler.didDisconnect();
+        Log.e("Disconnect","");
+        return error;
+    }
+
+    public boolean isConnected()
+    {
+        if(socket==null) return false;
+        else if(!socket.isConnected()) return false;
+        else return true;
+    }
+
+    public void write(final String data) {
+        try {
+            Log.d(TAG, "writ(): data = " + data);
+            out.write(data + "\n");
+            out.flush();
+        } catch (IOException ex) {
+            Log.e(TAG, "write(): " + ex.toString());
+        } catch (NullPointerException ex) {
+            Log.e(TAG, "write(): " + ex.toString());
+        }
+    }
+
+    public void disconnect() {
+        try {
+            Log.d(TAG, "Closing the socket connection.");
+
+            interrupted = true;
+            if(socket != null) {
+                socket.close();
+            }
+            if(out != null & in != null) {
+                out.close();
+                in.close();
+            }
+        } catch (IOException ex) {
+            Log.e(TAG, "disconnect(): " + ex.toString());
+        }
+    }
+}
---
+++ Android/LifeLinesMM2/app/src/main/java/com/lifelines/code/lifelinesmm2/MainActivity.java
@@ -0,0 +1,199 @@
+package com.lifelines.code.lifelinesmm2;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.OpenCVLoader;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+    AsyncConnection socketConnection = null;
+    Button sendButton;
+    ToggleButton tg_save;
+    EditText sendData;
+
+    TimerTask updateConnection;
+    Timer timer;
+
+    MainActivity myMain = null;
+
+    CheckBox rb_left,rb_leftRGB,rb_right,rb_rightRGB,rb_depth;
+
+    SharedPreferences.Editor editor;
+
+    String configs = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this,mCallBack); // Start OpenCV
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        editor = settings.edit();
+
+        myMain = this;
+        socketConnection = new  AsyncConnection("10.42.0.1",1234,1000,myMain);
+
+        rb_left = findViewById(R.id.rb_left);
+        rb_leftRGB = findViewById(R.id.rb_leftRGB);
+        rb_right = findViewById(R.id.rb_right);
+        rb_rightRGB = findViewById(R.id.rb_rightRGB);
+        rb_depth = findViewById(R.id.rb_depth);
+        tg_save = findViewById(R.id.tg_save);
+
+        configs = settings.getString("configs","00000");
+        setChecks(configs);
+        Log.e("configsRead:",configs);
+
+        startTimer(5);
+
+        sendButton = findViewById(R.id.sendButton);
+        sendData = findViewById(R.id.sendData);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendDataSocket(sendData.getText().toString());
+            }
+        });
+
+        tg_save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(tg_save.isChecked())
+                {
+                    sendDataSocket("startSaving");
+                }
+                else
+                {
+                    sendDataSocket("stopSaving");
+                }
+            }
+        });
+
+
+    }
+
+    public void onCheckButtonClicked(View view)
+    {
+        String sendConfigs = "setVideo";
+
+        if(rb_left.isChecked()) sendConfigs += "1";
+        else sendConfigs += "0";
+
+        if(rb_leftRGB.isChecked()) sendConfigs += "1";
+        else sendConfigs += "0";
+
+        if(rb_right.isChecked()) sendConfigs += "1";
+        else sendConfigs += "0";
+
+        if(rb_rightRGB.isChecked()) sendConfigs += "1";
+        else sendConfigs += "0";
+
+        if(rb_depth.isChecked()) sendConfigs += "1";
+        else sendConfigs += "0";
+
+        editor.putString("configs", sendConfigs);
+        editor.commit();
+
+        configs = sendConfigs;
+        sendDataSocket(sendConfigs);
+    }
+
+    class UpdateConnectionTask extends TimerTask {
+
+        public void run() {
+            if(socketConnection==null)
+            {
+                socketConnection.execute();
+                Log.i("Trying to connect","...");
+            }
+            else
+            {
+                if(!socketConnection.isConnected())
+                {
+                    socketConnection.disconnect();
+                    socketConnection = new  AsyncConnection("10.42.0.1",1234,1000,myMain);
+                    socketConnection.execute();
+                    Log.i("Trying to connect","...");
+                }
+                else
+                {
+                    //Log.i("Connected","...");
+                }
+            }
+
+        }
+    }
+
+    public void didReceiveData(String data)
+    {
+        Log.i("Received: ",data);
+    }
+
+    public void didDisconnect()
+    {
+        Log.i("didDisconnect: ","");
+    }
+
+    public void didConnect()
+    {
+        Log.i("didConnect: ","Connection ok");
+        sendDataSocket(configs);
+    }
+
+    private static final String TAG = "MainActivity";
+    BaseLoaderCallback mCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    break;
+                }
+                default:
+                {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    private void startTimer(int sec)
+    {
+        timer = new Timer();
+        updateConnection = new UpdateConnectionTask();
+        timer.scheduleAtFixedRate(updateConnection, 0, sec*1000);
+    }
+
+    private void sendDataSocket(String data)
+    {
+        if(socketConnection.isConnected()) socketConnection.write(data);
+    }
+
+    private void setChecks(String str)
+    {
+        str = str.replace("setVideo","");
+        if(str.charAt(0)=='1') rb_left.setChecked(true);
+        if(str.charAt(1)=='1') rb_leftRGB.setChecked(true);
+        if(str.charAt(2)=='1') rb_right.setChecked(true);
+        if(str.charAt(3)=='1') rb_rightRGB.setChecked(true);
+        if(str.charAt(4)=='1') rb_depth.setChecked(true);
+    }
+}
